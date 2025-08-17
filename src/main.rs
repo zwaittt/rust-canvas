1e898e2c4e6fe6b524c106ea038b0c386aadf69e
zwaittt <l20122005@live.com>
2025-08-17 20:21:44 +0800

    up

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let mut str = String::new();
+    let arr = [1, 2, 3, 4, 5];
+    std::io::stdin().read_line(&mut str)
+        .expect("should input something");
+    let idx: usize = str.trim()
+        .parse()
+        .expect("should input a number");
+    println!("you chosen value is {}", arr[idx]);
 }
